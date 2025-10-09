94c41bd71a074ffeff449d098d8c09b3dd08e26c
NyxB <bigler@simmons.edu>
2025-10-09 13:08:01 -0400

    Update helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
-
+#include <iostream> /* 
+^ this includes some basic functionality, we'll go into more detail later
+but it'll be part of pretty much all C++ projrams, as is the std namespace.
+*/
 using namespace std; //using standard namespace
 
 int main(){
     cout << "hello world";
     return 0;
 }
+
 
