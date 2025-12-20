b41119c1ebf7a0710e26a54c4af997806ea828d1
NyxB <bigler@simmons.edu>
2025-12-20 16:51:02 -0500

    Update helloWorld.cpp

--- PLP1 Intro/helloWorld.cpp
+++ PLP1 Intro/helloWorld.cpp
@@ -4,9 +4,10 @@ but it'll be part of pretty much all C++ projrams, as is the std namespace.
 */
 using namespace std; //using standard namespace
 
-int main(){
-    cout << "hello world";
-    return 0;
+int main(){  // Normally a function that doesn't return anything would be void, but main must return a value.
+    cout << "hello world"; // cout = "console out"
+    return 0; // Zero since we don't actually need to return anything
 }
+
 
 
