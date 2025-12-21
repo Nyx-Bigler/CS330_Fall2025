7ad10b2694afffbee6b23b44f83ea8a6461273ab
NyxB <bigler@simmons.edu>
2025-12-20 21:53:09 -0500

    Update Function_Code.cpp

--- PLP4 Functions/Function_Code.cpp
+++ PLP4 Functions/Function_Code.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <tuple>
+#include <string>
+
 using namespace std;
+
+// For tuples, I wound up using this resource quite a bit: https://www.geeksforgeeks.org/cpp/tuples-in-c
 
 // Multuplying two numbers
 int mult(int a, int b){return(a*b);}
@@ -13,7 +17,18 @@ if(x == 0 || x == 1) {return(1);}
 else {return(x * factorial(x-1));}
 }
 
-// String split
+// String Split (This is very rudementary, anything more sophisticated needs a tokenizer)
+tuple <string, string> split(string origional){
+
+    //start at index 0, take 5 characters
+    string part1 = origional.substr(0,5);
+
+    // start at index 6, take 6 characters
+    string part2 = origional.substr(6, 6); 
+
+    return(make_tuple(part1,part2));
+}
+
 
 // Reference or value? (the SWAP!)
 // Had to use tuples because functions can only return one value
@@ -42,6 +57,12 @@ int main() {
     // factorial:
     int fact = factorial(x);
     cout << "Factorial: " << fact <<endl;
+
+    // string split
+    string example = "Hello World!";
+    tuple <string, string> tupOutput = split(example); // can't just cout a tuple
+    cout << get<0>(tupOutput) <<endl; // gotta get the pieces
+    cout << get<1>(tupOutput) <<endl;
 
     // testing the swap
     cout << "x:  " << x <<" adr: "<< &x <<endl;
